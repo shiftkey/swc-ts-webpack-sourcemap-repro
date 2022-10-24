9027bed4ac7239c0d6d63cccbf813696cd764db9
Brendan Forster <github@brendanforster.com>
2022-10-24 15:34:04 -0300

    rename code

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import { StrictMode } from 'react'
 import {render} from 'react-dom'
 import { App } from './app'
 
-const appContainer = document.getElementById('memex-root')
+const appContainer = document.getElementById('project-root')
 
 render(<StrictMode><App /></StrictMode>, appContainer)
